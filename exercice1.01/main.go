1a96ad2d7203030002057bb9ce2d03dd6c12aa25
David GILLARD <ddavid.ggillard@protonmail.com>
2025-05-12 14:54:52 +0200

    ajout d'information au premier exercice en go

--- exercice1.01/main.go
+++ exercice1.01/main.go
@@ -7,9 +7,9 @@ import (
 	"time"
 )
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
-	r := rand.Intn(5)
-	stars := strings.Repeat("*", r)
-	fmt.Println(stars)
+func main() { // création de la fonction main
+	rand.Seed(time.Now().UnixNano()) // Alimentez le générateur de nombres aléatoires permettant de gerer le nombre d'étoile
+	r := rand.Intn(5)                // Générer un nombre aléatoire compris entre 0 et 1 pour obtenir un nombre compris entre 1 et 5
+	stars := strings.Repeat("*", r)  // Utiliser le répéteur de chaîne pour créer une chaîne contenant le nombre d'étoiles souhaité
+	fmt.Println(stars)               //Afficher la chaîne contenant les étoiles dans la console
 }
