c01726b7ec0b57f8ca8fa81a195063a87b19e4d7
David GILLARD <ddavid.ggillard@protonmail.com>
2025-05-23 16:07:53 +0200

    ajout d'activité sur le demande de prêt

--- 03-typage/activite3.02/main.go
+++ 03-typage/activite3.02/main.go
@@ -1,36 +1,59 @@
 package main
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 )
 
 const (
-	bonneCote := 450
-	faibleTauxCote := 10
-	bonTauxCote := 20
+	bonneCote      = 450
+	faibleTauxCote = 10
+	bonTauxCote    = 20
 )
 
 var (
 	ErrCoteCredit  = errors.New("Cote de crédit invalid")
-	ErrRevenu 	   = errors.New("Revenu Invalid")
+	ErrRevenu      = errors.New("Revenu Invalid")
 	ErrMontantPret = errors.New("Montant du prêt invalid")
 	ErrDureePret   = errors.New("Duée du prêt n'est pas divisible par 12")
 )
 
-
 func main() {
+	// approuvé
+	fmt.Println("Candidat 1")
+	fmt.Println("----------")
+	err := controlPret(500, 1000, 1000, 24)
+	if err != nil {
+		fmt.Println("Erreur: ", err)
+		return
+	}
+
+	// refusé
+	fmt.Println("Candidat 2")
+	fmt.Println("----------")
+	err = controlPret(350, 1000, 10000, 12)
+	if err != nil {
+		fmt.Println("Erreur: ", err)
+		return
+	}
+
+	fmt.Println("Candidat 3")
+	fmt.Println("----------")
+	err = controlPret(350, 3700, 250000, 12)
+	if err != nil {
+		fmt.Println("Erreur: ", err)
+		return
+	}
 
 }
 
-
 func controlPret(coteCredit int, revenu float64, montantPret float64, dureePret float64) error {
-	interets := 20
+	interets := 20.0
 	if coteCredit >= bonneCote {
 		interets = 15.0
-	
+
 	}
-	if  coteCredit < 1 {
+	if coteCredit < 1 {
 		return ErrCoteCredit
 	}
 
@@ -42,16 +65,15 @@ func controlPret(coteCredit int, revenu float64, montantPret float64, dureePret 
 		return ErrMontantPret
 	}
 
-	if dureePret < 1  || int(dureePret)%12 {
+	if dureePret < 1 || int(dureePret)%12 != 0 {
 		return ErrDureePret
 	}
 
 	taux := interets / 100
-
 	paiment := ((montantPret * taux) / dureePret) + (montantPret / dureePret)
 
 	//Coût total du crédit
-	InteretTotal := (paiment * dureePret) - montantPret
+	interetTotal := (paiment * dureePret) - montantPret
 
 	approuve := false
 
@@ -59,11 +81,21 @@ func controlPret(coteCredit int, revenu float64, montantPret float64, dureePret 
 		ratio := (paiment / revenu) * 100
 		if coteCredit >= bonneCote && ratio < bonTauxCote {
 			approuve = true
-		} else if ratio < faibleTauxCote  {
+		} else if ratio < faibleTauxCote {
 			approuve = false
 		}
 	}
 
-	fmt.Println("cote du crédit : ", )
-	
+	fmt.Println("Cote du crédit: ", coteCredit)
+	fmt.Println("Revenu: ", revenu)
+	fmt.Println("Montant du prêt: ", montantPret)
+	fmt.Println("Durée du prêt: ", dureePret)
+	fmt.Println("Paiment mensuel: ", paiment)
+	fmt.Println("Taux: ", interets)
+	fmt.Println("Cout total: ", interetTotal)
+	fmt.Println("Cote du crédit : ", coteCredit)
+	fmt.Println("Approuvé: ", approuve)
+	fmt.Println("")
+
+	return nil
 }
